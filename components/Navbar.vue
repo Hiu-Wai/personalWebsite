b4a1f894b6257551555d3a783f789a7262bf47b6
Hiu-Wai <chloeho248@gmail.com>
2024-12-09 21:59:14 +0000

    changed size of name

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <span class="fs-4" style = "color: white">Chloe Ho</span>
+          <span class="fs-2" style = "color: white;">Chloe Ho</span>
         </a>
         <ul class="nav col-12 col-lg-auto me-5 mb-2 justify-content-center mb-md-0">
             <li><NuxtLink to="/" class="nav-link px-3">Home</NuxtLink></li>
@@ -10,3 +10,6 @@
         </ul>
     </header>
 </template>
+
+<style>
+</style>
