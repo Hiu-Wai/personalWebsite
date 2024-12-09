1c1780111f9a2480e67d199aab213d223631294f
Hiu-Wai <chloeho248@gmail.com>
2024-12-09 16:43:36 +0000

    created default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <Navbar /> <!-- Include the navbar component -->
+      <slot  /> <!-- This will render the page content -->
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from '~/components/Navbar.vue'
+  
+  export default {
+    components: {
+      Navbar
+    }
+  }
+  </script>
+  
+  <style>
+  /* Add any global styles here */
+  </style>
+  
