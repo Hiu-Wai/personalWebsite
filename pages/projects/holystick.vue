932007a659066b710ed2656a6c839ca316285e2c
Hiu-Wai <chloeho248@gmail.com>
2024-12-09 19:22:25 +0000

    updated holystick description

--- pages/projects/holystick.vue
+++ pages/projects/holystick.vue
@@ -67,7 +67,7 @@
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                 </svg>
-                <span>Note: only a prototype of the final website. Data used is made up.</span>
+                <span>Note: Data used for this website is based on database provided, might not reflect reality.</span>
                 <div class="justify-content-between">
                   <a href="https://holystick.dylankainth.com/" class="btn btn-lg btn-primary m-2">Visit project deployment</a>
                   <a href="https://github.com/dylankainth/holystick" class="btn btn-lg btn-primary m-2">Visit Github for project</a>
