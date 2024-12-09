e8beb8f5fa5fe1a20909cd7ef50a16f0a0093f66
Hiu-Wai <chloeho248@gmail.com>
2024-12-09 21:19:09 +0000

    added about page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,6 +5,7 @@
         </a>
         <ul class="nav col-12 col-lg-auto me-5 mb-2 justify-content-center mb-md-0">
             <li><NuxtLink to="/" class="nav-link px-3">Home</NuxtLink></li>
+            <li><NuxtLink to="/about" class="nav-link px-3">About</NuxtLink></li>
             <li><NuxtLink to="/project" class="nav-link px-3">Projects</NuxtLink></li>
         </ul>
     </header>
