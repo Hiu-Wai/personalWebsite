73f443f0bc50eaf94da519b013c4d093a766d7cf
Hiu-Wai <chloeho248@gmail.com>
2024-12-14 10:55:45 +0000

    reformatted pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,26 +1,49 @@
 <template>
-    <body class="d-flex h-100 custom-bg">
+  <body class="d-flex h-100 custom-bg">
     <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-    <div class = "row">
-      <div class="col-lg-6 px-0">
-        <h1 class="display-4 fst-italic">A little about me...</h1>
-        <p class="lead my-3">I'm Chloe Ho (Ho Hiu Wai) and I am currently a computer science undergraduate at King's College London.</p>
-        <p class = "lead my-3">I enjoy coding due to its problem-solving nature and the satisfaction felt afterwards. But I am eager to explore the other different branches of computer science as it is such a big field. Feel free to contact me using the contact details mentioned in the home page!!</p>
-      </div>
-      <div class="col-lg-6 d-flex justify-content-end">
-          <img src = "\public\images\profile2.jpg" class = "custom-img">
+      <div class="row">
+        <div class="col-lg-6 px-0">
+          <h1 class="display-4 fst-italic">A little about me...</h1>
+          <p class="lead my-3">
+            I'm Chloe Ho (Ho Hiu Wai) and I am currently a computer science
+            undergraduate at King's College London.
+          </p>
+          <p class="lead my-3">
+            I enjoy coding due to its problem-solving nature and the
+            satisfaction felt afterwards. But I am eager to explore the other
+            different branches of computer science as it is such a big field.
+            Feel free to contact me using the contact details mentioned in the
+            home page!!
+          </p>
+        </div>
+        <div class="col-lg-6 d-flex justify-content-end">
+          <img
+            src="\public\images\profile2.jpg"
+            class="custom-img img-fluid rounded"
+          />
+        </div>
       </div>
     </div>
-    </div>
+    <br />
     <div class="row mb-2">
       <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-body-secondary">
+        <div
+          class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-body-secondary"
+        >
           <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-primary-emphasis">Education</strong>
-            <h3 class="mb-0 text-body-secondary">Cheltenham Ladies College 2017-2024</h3>
-            <h3 class="mb-0 text-body-secondary">King's College London 2024~</h3>
-            <br>
-            <strong class="d-inline-block mb-2 text-primary-emphasis">Achievements</strong>
+            <strong class="d-inline-block mb-2 text-primary-emphasis"
+              >Education</strong
+            >
+            <h3 class="mb-0 text-body-secondary">
+              Cheltenham Ladies College 2017-2024
+            </h3>
+            <h3 class="mb-0 text-body-secondary">
+              King's College London 2024~
+            </h3>
+            <br />
+            <strong class="d-inline-block mb-2 text-primary-emphasis"
+              >Achievements</strong
+            >
             <ul>
               <li>Gold in Senior Maths Challenge 2023</li>
               <li>Gold in Chemistry Olympiad 2024</li>
@@ -29,33 +52,45 @@
           </div>
         </div>
       </div>
-    <div class="col-md-6">
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-body-secondary">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary-emphasis">Participating societies</strong>
-          <ul>
-            <li>Womxn in STEM</li>
-            <li>Womxn in CS</li>
-            <li>KCL Tech</li>
-            <li>KCL Abacus</li>
-          </ul>
+      <div class="col-md-6">
+        <div
+          class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-body-secondary"
+        >
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary-emphasis"
+              >Participating societies</strong
+            >
+            <ul>
+              <li>Womxn in STEM</li>
+              <li>Womxn in CS</li>
+              <li>KCL Tech</li>
+              <li>KCL Abacus</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </body>
 </template>
 <style>
-.custom-img{
-    height:350px;
-    width:350px;
-    object-fit: contain;
-    overflow: hidden;
+.custom-img {
+  height: 100%;
+  width: 350px;
+  object-fit: contain;
+  overflow: hidden;
 }
-.h3{
-    color:black;
+.h3 {
+  color: black;
 }
-ul{
-  color:black;
+ul {
+  color: black;
+}
+.row {
+  display: flex;
+}
+.col-lg-6 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
--- pages/project.vue
+++ pages/project.vue
@@ -1,97 +1,120 @@
 <template>
-    <body class="d-flex h-100 text-center text-white custom-bg">
-      <div class="cover-container d-flex vw-100 vh-90 p-3 mx-auto flex-column">
-        <div class="d-flex justify-content-center">
-        <div class="p-5 mb-4 rounded-3 text-center bg-2" style = "width: 75%; background-color: #00312F;">
-        <div class="container-fluid py-5">
+  <body class="d-flex h-100 text-center text-white custom-bg">
+    <div class="cover-container d-flex vw-100 vh-90 p-3 mx-auto flex-column">
+      <div class="d-flex justify-content-center">
+        <div
+          class="p-5 mb-4 rounded-3 text-center bg-2"
+          style="width: 75%; background-color: #00312f"
+        >
+          <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Projects</h1>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
 
-        <main class="px-3">
-            <div class="album py-5">
-            <div class="container">
+      <main class="px-3">
+        <div class="album py-5">
+          <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col" v-for="(project,id) in projects" :key ="id">
-                    <div class="card h-100">
-                        <h2>{{ project.title }}</h2>
-                        <h5>{{ id }}</h5>
-                        <img class="bd-placeholder-img card-img-top img-fluid custom-size rounded mx-auto d-block" 
-                        width="100%" height="300" :src = "project.image" role="img" aria-label="Placeholder: Thumbnail" 
-                        preserveAspectRatio="xMidYMid slice" focusable="false" :style="getImageStyle(id)">
-                        <title>Placeholder</title><rect width="100%" height="100%" fill="#F5F9E9"></rect> 
-                        </img>
-                    <div class="card-body">
+              <div class="col" v-for="(project, id) in projects" :key="id">
+                <div class="card h-100">
+                  <h2>{{ project.title }}</h2>
+                  <h5>{{ id }}</h5>
+                  <img
+                    class="bd-placeholder-img card-img-top img-fluid custom-size rounded mx-auto d-block"
+                    width="100%"
+                    height="300"
+                    :src="project.image"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                    :style="getImageStyle(id)"
+                  />
+                  <title>Placeholder</title
+                  ><rect width="100%" height="100%" fill="#F5F9E9"></rect>
+                  <div class="card-body">
                     <p class="card-text" v-html="project.shortDescription"></p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                          <NuxtLink :to = "'/projects/' + id">
-                        <button type="button" class="btn btn-lg btn-outline-secondary">Learn more</button>
+                    <div
+                      class="d-flex justify-content-between align-items-center"
+                    >
+                      <div class="btn-group">
+                        <NuxtLink :to="'/projects/' + id">
+                          <button
+                            type="button"
+                            class="btn btn-lg btn-outline-secondary"
+                          >
+                            Learn more
+                          </button>
                         </NuxtLink>
-                        </div>
+                      </div>
                     </div>
-                    </div>
-                </div>   
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-            </div>
+          </div>
         </div>
-        </main>
-
-      </div>  
-    </body>
+      </main>
+    </div>
+  </body>
 </template>
 <script>
-import { projects } from '~/store/index.js';
-
 export default {
   data() {
     return {
-      projects,
+      projects: null, // Initialize as null to hold the fetched data
     };
+  },
+  mounted() {
+    // Access the Vuex store to fetch all projects
+    this.projects = this.$store.state.projects;
   },
   methods: {
     getImageStyle(id) {
       // Return a specific background color for the given ID
-      if (id === 'n0erng2887b0v0luu579') {
-        return { backgroundColor: '#00838f' };
+      if (id === "n0erng2887b0v0luu579") {
+        return { backgroundColor: "#00838f" };
       }
       // Default background color
-      return { backgroundColor: '#C7FFED' };
+      return { backgroundColor: "#C7FFED" };
     },
   },
 };
 </script>
 <style>
-    .custom-size{
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-        overflow: hidden;
-    }
-    .card{
-        background-color: #D1FFFD;
-        padding-left: 20px;
-        padding-right: 20px;
-        padding-top: 20px;
-    }
-    .container{
-        display: flex;
-        flex-wrap: wrap;
-        align-items: stretch;
-    }
-    .btn{
-        background-color: #d1ffd7;
-    }
-    .btn:hover, .btn:focus, .btn:active, .btn.active, .open>.dropdown-toggle.btn {
-    background-color: #B2DDF7;
-    }   
-    .bg-2{
-        background-image: url('C:\\Users\\Chloe\\Desktop\\Coding\\personalWebsite\\public\\images\\background-image2.jpg'); /* Path to your image */
-            background-size: cover; /* Make the image cover the whole page */
-            background-repeat: no-repeat; /* Prevent repeating */
-            background-position: center center; /* Center the image */
-            margin: 0; /* Remove default margins */
-    }
+.custom-size {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  overflow: hidden;
+}
+.card {
+  background-color: #d1fffd;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+}
+.btn {
+  background-color: #d1ffd7;
+}
+.btn:hover,
+.btn:focus,
+.btn:active,
+.btn.active,
+.open > .dropdown-toggle.btn {
+  background-color: #b2ddf7;
+}
+.bg-2 {
+  background-image: url("C:\\Users\\Chloe\\Desktop\\Coding\\personalWebsite\\public\\images\\background-image2.jpg"); /* Path to your image */
+  background-size: cover; /* Make the image cover the whole page */
+  background-repeat: no-repeat; /* Prevent repeating */
+  background-position: center center; /* Center the image */
+  margin: 0; /* Remove default margins */
+}
 </style>
