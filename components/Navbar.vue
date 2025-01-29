961be3dffda549a15c0dd912ef5cb9d0de8b154c
Hiu-Wai <chloeho248@gmail.com>
2025-01-29 12:40:19 +0000

    made collapsible navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
-<template>
-  <header
+<!-- <template>
+   <header
     class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
   >
     <a
@@ -18,11 +18,48 @@
       <li><NuxtLink to="/project" class="nav-link px-3">Projects</NuxtLink></li>
     </ul> 
   </header>
+</template> -->
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <a href="/" class="navbar-brand text-white fs-2">Chloe Ho</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <NuxtLink to="/" class="nav-link px-3" exact-active-class="exact-active" exact aria-current="page">Home</NuxtLink>
+            </li>
+            <li class="nav-item">
+              <NuxtLink to="/about" class="nav-link px-3" exact-active-class="exact-active" >About</NuxtLink>
+            </li>
+            <li class="nav-item">
+              <NuxtLink to="/project" class="nav-link px-3" exact-active-class="exact-active">Projects</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <style>
 .nav-link {
   color: #88ccf1;
+}
+.nav-link.exact-active {
+  color: #007bff;
 }
 @media (max-width: 576px) {
   body{
