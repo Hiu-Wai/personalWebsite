3ed436a42828f355cdb9838dd8b027324ac4c741
Hiu-Wai <chloeho248@gmail.com>
2025-01-29 08:53:50 +0000

    changed styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,27 +8,56 @@
     >
       <span class="name fs-2" style="color: white">Chloe Ho</span>
     </a>
-    <ul class="nav col-12 col-lg-auto me-5 mb-2 justify-content-center mb-md-0">
+  
+     <ul class="nav col-12 col-lg-auto me-5 mb-2 justify-content-center mb-md-0">
       <li>
         <NuxtLink to="/" class="nav-link px-3 active" aria-current="page"
-          >Home</NuxtLink
-        >
+          >Home</NuxtLink>
       </li>
       <li><NuxtLink to="/about" class="nav-link px-3">About</NuxtLink></li>
       <li><NuxtLink to="/project" class="nav-link px-3">Projects</NuxtLink></li>
-    </ul>
+    </ul> 
   </header>
 </template>
 
 <style>
 .nav-link {
-  font-size: 26px;
   color: #88ccf1;
 }
+@media (max-width: 576px) {
+  body{
+    font-size:14px;
+  }
+}
+
+/* Medium devices (tablets, 768px and up)*/
+@media (max-width: 768px) {
+  body{
+    font-size:17px;
+  }
+
+}
+
+/* Large devices (desktops, 992px and up) */
 @media (max-width: 992px) {
-  .nav-link {
-    text-align: center;
-    justify-content: center;
+  body{
+    font-size:20px;
+  }
+
+}
+
+/* X-Large devices (large desktops, 1200px and up) */
+@media (max-width: 1200px) {
+  body{
+    font-size:23px;
+  }
+
+}
+
+/* XX-Large devices (larger desktops, 1400px and up)*/
+@media (min-width: 1400px) {
+  body{
+    font-size:26px;
   }
 }
 </style>
