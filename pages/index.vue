cfe58e54a156e04c9a4c28e9fbe8b7c547c382c2
Hiu-Wai <chloeho248@gmail.com>
2025-01-30 11:54:24 +0000

    added triggerHoverEffect

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <main class="px-3">
         <h3>HELLO EVERYONE, I AM</h3>
         <h1>CHLOE HO (HIU WAI HO)</h1>
-        <div class="hover-overlay" id = "hoverOverlay">
+        <div class="hover-overlay" onclick='triggerHoverEffect(this)'>
           <img
             class="bd-placeholder-img rounded-circle profile-pic"
             src="public\images\profile.jpeg"
@@ -151,9 +151,16 @@
   opacity: 0;
   border-radius: 50%;
 }
+
 .hover-overlay:hover .mask{
   opacity: 1;
 }
+
+/* Active state for touch devices */
+.hover-overlay:active .mask {
+  opacity: 1;
+}
+
 @media (max-width: 576px) {
   .contact-details {
     background-size: cover;
@@ -201,23 +208,10 @@
 </style>
 
 <script>
-document.addEventListener('DOMContentLoaded', (event) => {
-  const hoverOverlay = document.getElementById('hoverOverlay');
-  const mask = hoverOverlay.querySelector('.mask');
-  
-  hoverOverlay.addEventListener('touchstart', () => {
-    mask.style.opacity = '1'; // Show the overlay on touch
-  });
-
-  document.addEventListener('touchstart', (e) => {
-    if (!hoverOverlay.contains(e.target)) {
-      mask.style.opacity = '0'; // Hide the overlay when tapping outside the image
-    }
-  });
-
-  hoverOverlay.addEventListener('touchend', () => {
-    mask.style.opacity = '0'; // Hide the overlay when the touch ends on the image
-  });
-});
+function triggerHoverEffect(element) {
+  element.classList.add("active");
+  setTimeout(() => {
+    element.classList.remove("active");
+  }, 500); // Removes the effect after 500ms
+}
 </script>
-
