537d9e55502f704bd0a6de00abf6852f4dc02175
Hiu-Wai <chloeho248@gmail.com>
2025-02-09 22:23:16 +0000

    added royal hackaway

--- pages/about.vue
+++ pages/about.vue
@@ -84,6 +84,7 @@
                 <li>KCL HackTheLion 2025</li>
                 <li>UCL Minerva's Hack 2025</li>
                 <li>KCL Tech4Good Hackathon 2025</li>
+                <li>Royal Hackaway 2025</li>
               </ul>
             </div>
           </div>
